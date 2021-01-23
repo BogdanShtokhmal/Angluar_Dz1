842a655d824ba86395ba4186e72b4a8be867c855
BogdanShtokhmal <sht.bogdan@gmail.com>
2021-01-23 18:19:37 +0200

    add dz task3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
+
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent
+    CarComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/Car.ts
@@ -0,0 +1,7 @@
+export interface Car{
+  model: string;
+  year: number;
+  power: number;
+  color: string;
+  driver: string;
+}
